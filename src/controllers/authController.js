581281e307203bef3baa83200a754ad34694809c
garsan84 <jgrsan84@gmail.com>
2021-06-19 23:15:11 +0200

    issue solved

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,26 +1,50 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
 
-const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const config = require('../config');
 
-router.post('/signup', async (req, res, next) => {
-    const { username, email, password} = req.body;
-    const user = new User ({
-        username: username,
-        email: email,
-        password: password
+const User = require("../models/User");
+
+router.post("/signup", async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const user = new User({
+    username,
+    email,
+    password
+  });
+  await user.save();
+  user.password = await user.encryptPassword(user.password);
+
+  const token = jwt.sign({id: user._id}, config.secret, {
+    expiresIn: 60 * 60 * 24
+  })
+  
+  res.json({auth: true, token});
+});
+
+router.post("/signin", (req, res, next) => {
+  res.json("signin");
+});
+
+router.get("/me", async (req, res, next) => {
+
+  const token = req.headers['x-access-token'];
+  if (!token) {
+    return res.status(401).json({
+      auth: false,
+      message: 'No token provided'
     });
-    user.password = await user.encryptPassword(user.password);
-    await user.save();
-    res.json({message: 'Received'})
-})
+  }
 
-router.post('/signin', (req, res, next) => {
-    res.json('signin');
-})
+  const decoded = jwt.verify(token, config.secret);
+  
+  const user = await User.findById(decoded.id);
+  if (!user) {
+    return res.status(404).send('No user found');
+  }
 
-router.get('/me', (req, res, next) => {
-    res.json('friday night bots');
-})
+  res.json(user);
+});
 
 module.exports = router;
--- src/models/User.js
+++ src/models/User.js
@@ -8,8 +8,8 @@ const userSchema = new Schema({
 });
 
 userSchema.methods.encryptPassword = async (password) => {
- const salt = await bcrypt.genSalt(10);
- bcrypt.hash(password, salt);
-}
+    const salt = await bcrypt.genSalt(10);
+    return bcrypt.hash(password, salt);
+};
 
 module.exports = model('User', userSchema);
